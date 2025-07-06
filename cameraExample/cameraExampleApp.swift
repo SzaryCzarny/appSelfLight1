6aa55807425c168dee9e11e1d7a9ce03c7b8d771
Arkadiusz SZURLEJ <szurlej24@gmail.com>
2025-07-06 15:25:41 +0200

    Initial Commit

---
+++ cameraExample/cameraExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  cameraExampleApp.swift
+//  cameraExample
+//
+//  Created by MacBook on 06/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct cameraExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
